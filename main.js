2af760a2c05ee82f8024f3031fe4d57803669de8
rasmus <rasmus.linde@voco.ee>
2025-12-08 19:43:40 +0200

    optimeeritud pildid

--- main.js
+++ main.js
@@ -7,18 +7,36 @@ import { allProductsView } from "/epood/All views/allProductsView.js";
 document.body.innerHTML += allProductsView;
 
 const products = [
-  new Product(1, "Sülearvuti Windows 11 Pro", 999.99, "Arvutid", "images/pilt.jpg"),
-  new Product(2, "iPhone 17", 1199.99, "Nutitelefonid", "images/pilt.jpg"),
-  new Product(3, "MacBook Pro 18", 2499.99, "Arvutid", "images/pilt.jpg"),
-  new Product(4, "Samsung Galaxy S26", 1099.99, "Nutitelefonid", "images/pilt.jpg"),
-  new Product(5, "AirPods Ultra", 399.99, "Tarvikud", "images/pilt.jpg"),
-  new Product(6, "Logitech MX Master 4", 99.99, "Tarvikud", "images/pilt.jpg"),
-  new Product(7, "Dell XPS 15", 1299.99, "Arvutid", "images/pilt.jpg"),
-  new Product(8, "Google Pixel 8", 899.99, "Nutitelefonid", "images/pilt.jpg"),
-  new Product(9, "Sony WH-1000XM5", 349.99, "Tarvikud", "images/pilt.jpg"),
-  new Product(10, "HP Spectre x360", 1399.99, "Arvutid", "images/pilt.jpg"),
-  new Product(11, "OnePlus 12", 799.99, "Nutitelefonid", "images/pilt.jpg"),
-  new Product(12, "Apple Watch Series 9", 499.99, "Tarvikud", "images/pilt.jpg"),
+  new Product(
+    1,
+    "Sülearvuti Windows 11 Pro",
+    999.99,
+    "Arvutid",
+    "images/pilt1.jpg"
+  ),
+  new Product(2, "iPhone 17", 1199.99, "Nutitelefonid", "images/pilt1.jpg"),
+  new Product(3, "MacBook Pro 18", 2499.99, "Arvutid", "images/pilt1.jpg"),
+  new Product(
+    4,
+    "Samsung Galaxy S26",
+    1099.99,
+    "Nutitelefonid",
+    "images/pilt1.jpg"
+  ),
+  new Product(5, "AirPods Ultra", 399.99, "Tarvikud", "images/pilt1.jpg"),
+  new Product(6, "Logitech MX Master 4", 99.99, "Tarvikud", "images/pilt1.jpg"),
+  new Product(7, "Dell XPS 15", 1299.99, "Arvutid", "images/pilt1.jpg"),
+  new Product(8, "Google Pixel 8", 899.99, "Nutitelefonid", "images/pilt1.jpg"),
+  new Product(9, "Sony WH-1000XM5", 349.99, "Tarvikud", "images/pilt1.jpg"),
+  new Product(10, "HP Spectre x360", 1399.99, "Arvutid", "images/pilt1.jpg"),
+  new Product(11, "OnePlus 12", 799.99, "Nutitelefonid", "images/pilt1.jpg"),
+  new Product(
+    12,
+    "Apple Watch Series 9",
+    499.99,
+    "Tarvikud",
+    "images/pilt1.jpg"
+  ),
 ];
 
 const laptop = products[0];
@@ -44,7 +62,7 @@ function renderProducts() {
   const container = document.querySelector("#product-list");
   container.innerHTML = "";
 
-  products.forEach(p => {
+  products.forEach((p) => {
     const card = document.createElement("div");
     card.className = "product-card";
 
